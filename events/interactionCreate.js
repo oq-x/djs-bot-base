9ed811976795e2231a16871a3fec38eceb9bca0a
XcHeeZ <84847714+xcheez1@users.noreply.github.com>
2022-07-14 00:44:26 +0300

    Command handler

---
+++ events/interactionCreate.js
@@ -0,0 +1,49 @@
+const { CommandInteraction, Client, Collection } = require("discord.js")
+const ms = require("ms")
+const prettyMilliseconds = require("pretty-ms")
+
+module.exports = {
+    name: "interactionCreate",
+    /**
+     * @param {Client} client
+     * @param {CommandInteraction} interaction
+     */
+    async execute(client, interaction){
+        if(interaction.isCommand()){
+            const command = client.commands.get(interaction.commandName)
+            if(!interaction.guild || !interaction.member) return await interaction.reply({ content: "This command must be executed in a guild.", ephemeral: true })
+            if(!command) return await interaction.reply({ content: "Unknown command.", ephemeral: true })
+            const missingPermissions = []
+            for(const permission of command.permissions){
+                if(!interaction.member.permissions.has(permission)) missingPermissions.push(permission)
+            }
+            if(missingPermissions.length) return await interaction.reply({ content: `You do not have enough permissions to use this command. Missing permissions: ${missingPermissions.map(perm => `\`${perm}\``).join(", ")}`, ephemeral: true })
+
+            if(!client.cooldowns.has(command.data.name)){
+                client.cooldowns.set(command.data.name, new Collection())
+            }
+            const currentTime = Date.now()
+    
+            let timestamps = client.cooldowns.get(command.name)
+            const cooldownAmount = ms(command.cooldown)
+            if(timestamps.has(message.author.id)){
+                const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount
+    
+                if(currentTime < expirationTime){
+                    const timeLeft = prettyMilliseconds(expirationTime - currentTime, { compact: true, verbose: true })
+    
+                    return await interaction.reply({ content: `Please wait ${timeLeft} before running this command again.`, ephemeral: true })
+                }
+            }
+
+            try {
+                await command.execute(interaction)
+                timestamps.set(interaction.user.id, currentTime)
+            } catch(content) {
+                console.log(content)
+                await interaction.reply({ content, ephemeral: true })
+            }
+
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,20 +1,27 @@
+//Importing Packages
 const { Client, Collection } = require("discord.js")
 const fs = require("fs")
 const { QuickDB } = require("quick.db")
 const config = require("./config.json")
 const { REST } = require("@discordjs/rest")
+
+// Add properties to env
 for(const i in config){
     process.env[i] = config[i]
 }
 
 const commands = []
 
+// Create the bot, api client and commands collection
 const client = new Client({ intents: process.env.intents })
 const api = new REST().setToken(process.env.token)
 const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"))
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"))
 client.commands = new Collection()
+client.cooldown = new Collection()
 
+
+// Add commands and events
 for(const file of eventFiles){
     const event = require(file);
     client.on(event.name, event.execute.bind(null, client))
@@ -26,12 +33,13 @@ for(const file of commandFiles){
     commands.set(command.data.name, command)
 }
 
+// Register application commands
 (async() => {
     await api.put(`https://discord.com/api/${process.env.version}/applications/${process.env.id}/commands`)
     console.log(`Successfully registered application commands.`)
 })
 
-
+// Finally, start the bot and export all of the neccessary objects
 client.login(process.env.token)
 exports.client = client;
 exports.db = new QuickDB();
